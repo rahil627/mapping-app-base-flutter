c6fe294bed2d00bbefb0abad4400bd3793de7114
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-08-30 01:31:33 -0700

    paste in the google map "state"

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// => only for single line expressions?? useless??
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+// NOTE: StatefulWidget vs StatelessWidget
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MapScreen();
+}
+
+class _MapScreen extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BaseScreen extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
